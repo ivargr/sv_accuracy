61447835cf78150731ad89a95fd25c7b1553a6bb
Ivar Grytten <ivar.grytten@gmail.com>
2023-11-30 14:28:55 +0100

    Pipeline

--- sv_accuracy/sv_accuracy.py
+++ sv_accuracy/sv_accuracy.py
@@ -5,10 +5,19 @@ def get_accuracy(truth_vcf_file_name: str, sample_vcf_file_name: str):
     return calculate_accuracy(truth, calls)
 
 
+def normalize_genotype(genotype):
+    genotype = genotype.replace("|", "/")
+    if genotype == "1/0":
+        genotype = "0/1"
+
+    return genotype
+
+
 def calculate_accuracy(truth, calls):
-    truth = {(variant.id, variant.position, variant.ref_seq, variant.alt_seq): variant.genotypes for variant in truth.tolist()}
+    truth = {(variant.chromosome, variant.position, variant.ref_seq, variant.alt_seq): variant.genotypes for variant in truth.tolist()}
     success = 0
     for variant in calls.tolist():
-        if variant.genotypes == truth[(variant.id, variant.position, variant.ref_seq, variant.alt_seq)]:
+        if normalize_genotype(variant.genotypes) == \
+            normalize_genotype(truth[(variant.chromosome, variant.position, variant.ref_seq, variant.alt_seq)]):
             success += 1
     return success/len(truth)
